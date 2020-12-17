c77717d4c4633e5ddf4b535e56560a53b3f6a3c9
V7hinc <V7hinc>
2020-12-17 21:07:22 +0800

    优化破解过程

--- README.md
+++ README.md
@@ -8,16 +8,7 @@ docker pull v7hinc/nessus_crack
 docker run -name nessus_crack -p 8834:8834 -dit v7hinc/nessus_crack
 ```
 
-## 第一次访问
-打开浏览器访问https://IP:8834，进入到初始化选择安装界面
-这里我们要选择 Managed Scanner。
-点击继续，然后这里一定要选择Tenable.sc。
-然后设置用户名密码
-这时候看到页面只有setting，没有scan
-
-## 执行安装插件代码
-```shell script
-
-```
-打开浏览器访问https://IP:8834，进入插件编译过程，这个进度会比较慢
-如果编译插件过程中发生无法访问情况可执行```/etc/init.d/nessusd restart```
+## 访问方式
+使用浏览器访问 https:IP:8834
+账号：admin
+密码：nimda
--- nessus_update_plugins_crack.sh
+++ nessus_update_plugins_crack.sh
@@ -1,52 +1,18 @@
-#!/usr/bin/env bash
-echo "执行的参数为$1"
-
-nessus_restart(){
-  /etc/init.d/nessusd restart
-}
-
-download_plugins(){
-  # 获取下载插件下载的链接，这个网上有很多就是根据activation code和challenge code获取，我这边直接一步到位
-  download_plugins_url="https://plugins.nessus.org/v2/nessus.php?f=all-2.0.tar.gz&u=91010fb015bb6e1040e3d332a2880d03&p=b527b2904f04ed6ca73541f2e4976554";
-  # 下载nessus的插件包
-  curl -k -o all-2.0.tar.gz $download_plugins_url;
-}
-update_plugins(){
-  # 如果你已经下载好了插件包，就可以直接执行下面的步骤
-  # 更新插件，这个需要一会儿时间
-  /opt/nessus/sbin/nessuscli update all-2.0.tar.gz;
-  # 更新完了，删除插件包
-  rm -f all-2.0.tar.gz;
-}
-crack(){
-  # 获取PLUGIN_SET
-  PLUGIN_SET=$(curl "https://plugins.nessus.org/v2/plugins.php");
-  crack_file=/opt/nessus/var/nessus/plugin_feed_info.inc;
-  # 将必要的内容写入plugin_feed_info.inc中实现破解
-  echo 'PLUGIN_SET = "'$PLUGIN_SET'";' > $crack_file;
-  echo 'PLUGIN_FEED = "ProfessionalFeed (Direct)";' >> $crack_file;
-  echo 'PLUGIN_FEED_TRANSPORT = "Tenable Network Security Lightning";' >> $crack_file;
-}
-
-main()
-{
-if [[ $1 = 'download_plugins' ]];
-then
-  download_plugins
-elif [[ $1 = 'update_plugins' ]];
-then
-  update_plugins
-elif [[ $1 = 'crack' ]];
-then
-  crack
-elif [[ $1 = 'restart' ]];
-then
-  nessus_restart
-else
-  download_plugins
-  update_plugins
-  crack
-  nessus_restart
-fi
-}
-main "$1"
+set -x;\
+# 获取下载插件下载的链接，这个网上有很多就是根据activation code和challenge code获取，我这边直接一步到位
+download_plugins_url="https://plugins.nessus.org/v2/nessus.php?f=all-2.0.tar.gz&u=91010fb015bb6e1040e3d332a2880d03&p=b527b2904f04ed6ca73541f2e4976554";\
+# 下载nessus的插件包
+curl -k -o all-2.0.tar.gz $download_plugins_url;\
+# 如果你已经下载好了插件包，就可以跳过上面2步，直接执行下面的步骤
+# 更新插件，这个需要一会儿时间
+/opt/nessus/sbin/nessuscli update all-2.0.tar.gz;\
+# 更新完了，删除插件包
+rm -f all-2.0.tar.gz;\
+# 获取PLUGIN_SET
+PLUGIN_SET=$(curl "https://plugins.nessus.org/v2/plugins.php");\
+crack_file=/opt/nessus/var/nessus/plugin_feed_info.inc;\
+# 将必要的内容写入plugin_feed_info.inc中实现破解
+echo 'PLUGIN_SET = "'$PLUGIN_SET'";' > $crack_file;\
+echo 'PLUGIN_FEED = "ProfessionalFeed (Direct)";' >> $crack_file;\
+echo 'PLUGIN_FEED_TRANSPORT = "Tenable Network Security Lightning";' >> $crack_file;\
+/etc/init.d/nessusd restart
